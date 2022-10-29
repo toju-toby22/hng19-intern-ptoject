110f6f81d2e69067a3e2635dfd43afe3c8914962
Tobi Otoju <ttotoju@gmail.com>
2022-10-29 21:10:51 +0100

    Updates

---
+++ precache-manifest.6bc8668a44a8c81d97dee00305f2ed18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcf9539ed831e1e3c81",
+    "url": "/static/css/main.2ec5debe.chunk.css"
+  },
+  {
+    "revision": "9fcf9539ed831e1e3c81",
+    "url": "/static/js/main.9fcf9539.chunk.js"
+  },
+  {
+    "revision": "bfa36157bcc75fb8ea07",
+    "url": "/static/js/1.bfa36157.chunk.js"
+  },
+  {
+    "revision": "d58e3843a90c6656e3a5",
+    "url": "/static/js/2.d58e3843.chunk.js"
+  },
+  {
+    "revision": "0da2dd41623d9513095b",
+    "url": "/static/js/runtime~main.0da2dd41.js"
+  },
+  {
+    "revision": "7e31ec7f651be70e5188f9d5ec324f86",
+    "url": "/static/media/profile__img.7e31ec7f.png"
+  },
+  {
+    "revision": "359a2237aef7c5194a1e6c8189db4dac",
+    "url": "/index.html"
+  }
+];
